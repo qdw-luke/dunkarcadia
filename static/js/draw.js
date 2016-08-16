67d67f1af8f79e5ea16d8fcfb77bc364e46c8041
Luke Shulman <Luke Shulman>
2016-08-16 17:11:17 -0400

    style changes

--- static/js/draw.js
+++ static/js/draw.js
@@ -34,10 +34,10 @@ function draw(donationData, employeeData, nameOverride, donationsJson) {
     resizeLeaderboard();
 
     function resizeLeaderboard() {
-        $("#leaderboard").css("width", Math.max(size, ($(".leaderboard-holder").width() *.8))+"px");
-        $("#leaderboard").css("margin-left", ($("#leaderboard").width()<size+40?0:$("#leaderboard").width()%size/2)+"px")
-            .css("width", (Math.floor($("#leaderboard").width()/size)*size)+"px");
-    }
+       $("#leaderboard").css("width", Math.max(size, ($(".leaderboard-holder").width() *.8))+"px");
+       $("#leaderboard").css("margin-left", ($("#leaderboard").width()<size+40?0:$("#leaderboard").width()%size/2)+"px")
+           .css("width", (Math.floor($("#leaderboard").width()/size)*size)+"px");
+   }
 
     var titleMap = {}, picMap = {}, slackMap = {}, middleMap = {}, nameList = [];
     employeeData.forEach(function(d){
